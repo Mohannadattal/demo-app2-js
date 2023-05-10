e279a1244baa6450ad901170485cbe27b13da97f
Mohannadgithub <mohannadattal85@gmail.com>
2023-05-10 15:12:21 +0200

    for-in Loops & Outputting Dynamic Properties

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -13,7 +13,13 @@ const renderMovies = () => {
   movieList.innerHTML = '';
   movies.forEach((movie) => {
     const movieEl = document.createElement('li');
-    movieEl.textContent = movie.info.title;
+    let text = movie.info.title + ' - ';
+    for (const key in movie.info) {
+      if (key !== 'title') {
+        text += `${key}: ${movie.info[key]}`;
+      }
+    }
+    movieEl.textContent = text;
     movieList.append(movieEl);
   });
 };
